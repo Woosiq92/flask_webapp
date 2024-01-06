ad66596359fa9704d8f3c57bd8deff94fdd1d182
Woosiq92 <34748897+Woosiq92@users.noreply.github.com>
2024-01-06 21:10:26 +0900

    Update app.py

--- dbapp/app.py
+++ dbapp/app.py
@@ -3,7 +3,6 @@ from pathlib import Path
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
-from flask_login import LoginManager
 
 csrf = CSRFProtect() 
 db = SQLAlchemy()
